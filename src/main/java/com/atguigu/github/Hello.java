cd30e8c1e8cb0f80dedcee34a1c08c5f4a2e7573
ljqstudy <ljqstudy@163.com>
2020-07-04 22:16:01 +0800

    第二次提交

--- src/main/java/com/atguigu/github/Hello.java
+++ src/main/java/com/atguigu/github/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
         System.out.println("hello a!");
         System.out.println("hello a!");
         System.out.println("hello a!");
+        System.out.println("hello a!");
+        System.out.println("hello a!");
     }
 }
--- src/main/java/com/atguigu/github2/Hello2.java
+++ src/main/java/com/atguigu/github2/Hello2.java
@@ -3,5 +3,7 @@ package com.atguigu.github2;
 public class Hello2 {
     public static void main(String[] args) {
         System.out.println("hello2");
+        System.out.println("hello2");
+        System.out.println("hello2");
     }
 }
