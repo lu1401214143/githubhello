00187e4f6643b7439fb0e2b9438e4fd8036ef077
lu1401214143 <1401214143@qq.com>
2020-07-04 14:46:41 +0800

    hotfix第一次提交

--- src/main/java/com/atguigu/github/Hello.java
+++ src/main/java/com/atguigu/github/Hello.java
@@ -3,5 +3,8 @@ package com.atguigu.github;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello a!");
+        System.out.println("hello a!");
+        System.out.println("hello a!");
+        System.out.println("hello a!");
     }
 }
