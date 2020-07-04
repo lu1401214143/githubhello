930e254307059a8240b085c3f5854d5a66d3dbfc
ljqstudy <ljqstudy@163.com>
2020-07-04 22:19:59 +0800

    第二次提交

---
+++ src/main/java/com/atguigu/Hello3.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+public class Hello3 {
+    public static void main(String[] args) {
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+    }
+}
